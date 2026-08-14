3379ac53464409001bdaf11365166e8a271a912d
Chaitanya Bhore <chaitanyabhore368@gmail.com>
2026-08-14 16:23:27 +0530

    Completed Leave Management Module

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+        [Required]
+        public int? NumberOfDays { get; set; }
+    }
+}
